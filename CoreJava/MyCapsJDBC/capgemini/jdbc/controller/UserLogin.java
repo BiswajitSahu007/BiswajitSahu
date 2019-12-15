554ffd01c5c4c595a77311d18d5a3f4b0112f4ae
BiswajitSahu007 <sahubiswajit430@gmail.com>
2019-12-15 10:57:03 +0530

    JDBC

--- CoreJava/MyCapsJDBC/capgemini/jdbc/controller/UserLogin.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.jdbc.controller;
-
-import java.util.Scanner;
-
-public class UserLogin {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-	}
-}
